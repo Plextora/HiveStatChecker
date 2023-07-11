6bb70b9f36c4e5d5750ac1f8339081029adfd66b
Plextora <71889427+Plextora@users.noreply.github.com>
2023-07-10 21:32:12 -0400

    Created map with hive gamemodes and their corresponding API name

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,22 @@ script.description = "Check Hive statistics in-game";
 script.version = "0.0.0";
 script.author = "Plextora";
 
+const hiveGamemodes: Map<string, string> = new Map<string, string>([
+  ["Treasure Wars", "wars"],
+  ["Deathrun", "dr"],
+  ["Hide and Seek", "hide"],
+  ["Survival Games", "sg"],
+  ["Murder Mystery", "murder"],
+  ["Skywars", "sky"],
+  ["Capture the Flag", "ctf"],
+  ["Block Drop", "drop"], // this is a gamemode?? never heard of it.
+  ["Ground Wars", "ground"],
+  ["Just Build", "build"],
+  ["Block Party", "party"],
+  ["The Bridge", "bridge"],
+  ["Gravity", "gravity"],
+]);
+
 client.showNotification(`Script ${script.name} has been loaded!`);
 
 let hiveStatCheckerModule: Module = new Module(
