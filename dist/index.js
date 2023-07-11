858b55c24c5ac5fb939977e6b06576c657cef6d6
Plextora <71889427+Plextora@users.noreply.github.com>
2023-07-10 21:09:30 -0400

    module stuff

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
+require("network");
 script.name = "Hive Stat Checker";
 script.description = "Check Hive statistics in-game";
 script.version = "0.0.0";
 script.author = "Plextora";
+client.showNotification(`Script ${script.name} has been loaded!`);
+let hiveStatCheckerModule = new Module("HiveStatChecker", script.name, script.description, 0 /* KeyCode.None */);
+client.getModuleManager().registerModule(hiveStatCheckerModule);
+client.on("unload-script", (ev) => {
+    if (ev.scriptName === script.name) {
+        client.getModuleManager().deregisterModule(hiveStatCheckerModule);
+    }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,23 @@
+const network = require("network");
+
 script.name = "Hive Stat Checker";
 script.description = "Check Hive statistics in-game";
 script.version = "0.0.0";
 script.author = "Plextora";
+
+client.showNotification(`Script ${script.name} has been loaded!`);
+
+let hiveStatCheckerModule: Module = new Module(
+  "HiveStatChecker",
+  script.name,
+  script.description,
+  KeyCode.None
+);
+
+client.getModuleManager().registerModule(hiveStatCheckerModule);
+
+client.on("unload-script", (ev) => {
+  if (ev.scriptName === script.name) {
+    client.getModuleManager().deregisterModule(hiveStatCheckerModule);
+  }
+});
