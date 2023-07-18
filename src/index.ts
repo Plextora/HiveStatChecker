dcb74ba84bdca70698bae8117b6bf440bd3699cf
Plextora <71889427+Plextora@users.noreply.github.com>
2023-07-18 12:24:18 -0400

    this looks better

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Credits: rosie for doing this idea before I ever could :(
 
-import { cmdPrefix, cmds } from "./cmds/cmdHandler";
+import * as cmdHandler from "./cmds/cmdHandler";
 
 script.name = "Hive Stat Checker";
 script.description = "Check Hive statistics in-game";
@@ -26,11 +26,11 @@ client.on("unload-script", (ev) => {
 
 client.on("send-chat", (ev) => {
   if (hiveStatCheckerModule.isEnabled()) {
-    if (ev.message.startsWith(cmdPrefix)) {
+    if (ev.message.startsWith(cmdHandler.cmdPrefix)) {
       ev.cancel = true;
       const args: string[] = ev.message.substring(1).split(" ").slice(1);
       ev.message = ev.message.substring(1).split(" ")[0];
-      for (const [key, value] of cmds.entries()) {
+      for (const [key, value] of cmdHandler.cmds.entries()) {
         if (ev.message === value) {
           key(args);
         }
