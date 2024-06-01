2324e135c0a59ca10c87cbefec5effb5882f1672
Plextora <71889427+Plextora@users.noreply.github.com>
2024-06-01 19:16:35 -0400

    changed cmdPrefix to -

--- src/cmds/cmdHandler.ts
+++ src/cmds/cmdHandler.ts
@@ -2,7 +2,7 @@ import help from "./help";
 import getAllTimeLB from "./allTime/getAllTimeLB";
 import getAllTimePlayerStats from "./allTime/getAllTimePlayerStats";
 
-export let cmdPrefix = "*";
+export let cmdPrefix = "-";
 export let cmds: Map<Function, string> = new Map<Function, string>([
   [help, "help"],
   [getAllTimeLB, "get-all-time-lb"],
