0fb93447bdefc359057a15d837ebb5389ac5b7cb
Plextora <71889427+Plextora@users.noreply.github.com>
2024-06-02 16:00:08 -0400

    Help command header

--- src/cmds/help.ts
+++ src/cmds/help.ts
@@ -3,6 +3,11 @@ import { cmdPrefix } from "./cmdHandler";
 export default function help(args: string[]) {
   clientMessage(
     decodeURI(
+      "\u00A7l\u00A79Hive Stat Checker Commands:\n-----------------------"
+    )
+  );
+  clientMessage(
+    decodeURI(
       `\u00A7c${cmdPrefix}\u00A7l\u00A76get-all-time-lb\n\u00A7r\u00A7eSyntax: \u00A7fget-all-time-lb <GameMode> <NumberOfPositionsShown>`
     )
   );
