ceb8bbef50d6a440dcb29ea1cca35f2058b119bf
Plextora <71889427+Plextora@users.noreply.github.com>
2024-06-01 20:49:31 -0400

    fix dumb variable naming

--- src/cmds/allTime/getAllTimeLB.ts
+++ src/cmds/allTime/getAllTimeLB.ts
@@ -2,15 +2,18 @@ import gameModeToGameCode from "../../util/gameModeToGameCode";
 const network = require("network");
 
 export default function getAllTimeLB(args: string[]) {
-  let gameMode: string = gameModeToGameCode(args[0])!;
+  let gameMode: string = args[0];
+  let gameCode: string = gameModeToGameCode(gameMode)!;
   let numberOfPositions: number = Number(args[1]);
 
-  let request = network.get(`https://api.playhive.com/v0/game/all/${gameMode}`);
+  let request = network.get(`https://api.playhive.com/v0/game/all/${gameCode}`);
 
   if (request.statusCode === 200) {
     const response: any = JSON.parse(util.bufferToString(request.body));
     clientMessage(
-      decodeURI(`\u00A7l\u00A76${args[0]} Leaderboard\n-----------------------`)
+      decodeURI(
+        `\u00A7l\u00A76${gameMode} Leaderboard\n-----------------------`
+      )
     );
     for (const i in response) {
       if (numberOfPositions < response[i].human_index) {
